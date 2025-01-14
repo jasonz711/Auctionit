ac33f771b06cff918248b92e78ef55e40f027678
Jason Zhang <jason.zhang@gentrack.com>
2025-01-14 22:05:09 +1100

    add synchronous communication

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,13 +2,15 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+
 
 var app = builder.Build();
 
