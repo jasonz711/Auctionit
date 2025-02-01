69ee969e58c8bef3777d9bf8584bb86207da500c
Jason Zhang <jason.zhang@gentrack.com>
2025-02-01 15:24:00 +1100

    bug fix for bid placed event

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -14,10 +15,12 @@ namespace BiddingService.Controllers
     public class BidsController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public BidsController(IMapper mapper)
+        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
         [Authorize]
         [HttpPost]
@@ -66,6 +69,8 @@ namespace BiddingService.Controllers
             }
 
             await DB.SaveAsync(bid);
+
+            await _publishEndpoint.Publish(_mapper.Map<BidPlaced>(bid));
 
             return Ok(_mapper.Map<BidDto>(bid));
         }
